50ac6228ff984e39a914514ab5497285f641bac8
Manohar-Gunturu <gunturumanohar2@gmail.com>
2018-01-02 00:15:57 -0500

    Added Polymer 2 support with es6 class

---
+++ calendar-lite.js
@@ -0,0 +1,331 @@
+class CalendarLite extends Polymer.GestureEventListeners(Polymer.Element){
+	
+	static get is() { return 'calendar-lite'; }
+      
+	  static get properties() {
+	    return {
+			date: {
+			  type: Date,
+			},
+			currentMonth: {
+				type: Number
+			  },
+			 minDate: {
+			  type: Date,
+			  value: null
+			},
+			maxDate: {
+			  type: Date,
+			  value: null
+			},
+			disabledDays:{
+			  type:Array,
+			  value:[]	
+			},
+			currentYear: {
+			  type: Number
+			},
+			mainColor:{
+			  type: String,
+			  value: null
+			},
+			multiSelect:{
+			   type:Object,
+			   value:null
+			},
+			days:{
+			  type:Array
+			},
+			separator:{
+			  type:Array,
+			  value:[0,1,2,3,4,5]
+			},
+			disabledWeekDay:{
+			  type:Array,
+			  value:[]	
+			}
+	  }
+
+
+	  }	  
+	
+	  static get observers() {
+        return [
+        '_populate(date)'
+		]
+	  }
+	
+	constructor() {
+		super();
+		this.is =  'calendar-lite';
+		this.days_names = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+		this.months_names = ['Jan','Feb','Mar','April','May','June','July','Aug','Sep','Oct','Nov','Dec'];
+		// temp variables used in logic to generate calendar
+		this.current_page = '';
+	  
+	  this.multiple = [];
+	  this.tmpDate = null;
+	  this.cf = null;
+	  this.tmpObject=null;  
+	}
+	
+	//to get number of days in a month 
+   monthDays(date){
+		 return (new Date(date.getFullYear(), date.getMonth()+1, 0)).getDate();	
+   }
+
+   ready() {
+      // generate 6 x 7 table
+        super.ready(); // for 2.0 class-based elements only
+
+	  this.generateTable();
+      this._animationEvent = this._whichAnimationEnd();
+      
+      this.multiple.push(this.date.getDate()+","+this.date.getMonth()+","+this.date.getFullYear());
+
+      this.currentYear = this.date.getFullYear();
+      this.currentMonth = this.date.getMonth();  
+	  
+	  //update header color if set
+	  if(this.mainColor != null){
+         this.customStyle['--my-elem-primary'] = this.mainColor;
+         this.updateStyles();
+      }
+	  
+	  //push into years list
+	  var tmpArray = [];
+	  if(this.maxDate != null && this.minDate != null){
+          this._generateYears((this.minDate).getFullYear(),(this.maxDate).getFullYear())   
+	  }else{
+		  this._generateYears(this.currentYear-101, this.currentYear+30 ,tmpArray)
+	  }
+     //discard tmpArray
+     tmpArray = null;
+  }
+  
+  _getUpdated(d,type){
+    if(type == 'year'){
+       return this.date.getFullYear();
+    }else if(type == 'month'){
+       return this.months_names[this.date.getMonth()];
+    }
+    else if(type == 'day'){
+       return this.days_names[this.date.getDay()]
+    }else if(type == 'date'){
+       return this.date.getDate();
+    }else{
+      //some thing weird
+    }
+  }
+  
+  generateTable(){
+
+      // if date attribute is not set take today date
+      if(this.date == undefined){
+        this.date = new Date();
+      }     
+       
+	  //clone into tmpDate  
+      this.tmpDate = new Date(this.currentYear,this.currentMonth,1);    
+      
+	  //tmpArray contains 6x7(42) items 
+	  var tmpArray = [];
+	  this.cf = 0;
+	  //fill with empty cells	  
+      for(var i=0;i<this.tmpDate.getDay();i++){
+        tmpArray.push({text:"",isDisabled:false,i:this.cf++});
+      }
+	  
+	  //fill days and check disable dates
+      for(var i=1;i<=this.monthDays(this.tmpDate);i++){
+        this.tmpDate.setDate(i);
+        if((this.minDate != null && this.tmpDate<=this.minDate) || (this.maxDate != null && this.tmpDate>=this.maxDate) || this.disabledWeekDay.indexOf(this.days_names[(this.tmpDate).getDay()]) != -1 || (this.disabledDays).indexOf(i) != -1){
+          tmpArray.push({text:i,isDisabled:true,i:this.cf++});
+        }else{
+          tmpArray.push({text:i,isDisabled:false,i:this.cf++});
+        }
+         
+      }
+	  //fill remaining empty cells
+      this.cf = (tmpArray.length>35)?(42-(tmpArray.length)):(34-(tmpArray.length));
+      for(var j=0;j<=this.cf;j++){
+        tmpArray.push({text:""});
+      }     
+      this.days = tmpArray;
+      tmpArray = null;
+    }
+    
+	_getDayClass(s,d){
+     if(this.multiSelect != null){
+         if(this.multiple.indexOf(s+","+this.currentMonth+","+this.currentYear) > -1){
+             return "dateSticker  selected";
+         }
+      }
+      if(this.date.getDate() == s && this.date.getMonth() == this.currentMonth && this.date.getFullYear()== this.currentYear){
+      return "dateSticker selected"; 
+    }
+     return "dateSticker";
+   }
+    _setDate(e){
+      var target = e.target;
+      var f = e.model.day;    
+      if(f.text != "" && !e.model.day.isDisabled){
+        
+        if(this.multiSelect != null){
+        if(this.multiSelect.consequent){
+          this.multiple = [];
+          this.cf = f.i;
+          this.multiple.push(f.text+","+this.currentMonth+","+this.currentYear)
+          for(var j=1;this.multiple.length<(this.multiSelect.max);j++){
+            this.tmpDate = new Date(this.currentYear,this.currentMonth,f.text+j);
+            if((this.minDate != null && this.tmpDate<=this.minDate) || (this.maxDate != null && this.tmpDate>=this.maxDate) || this.disabledWeekDay.indexOf(this.days_names[(this.tmpDate).getDay()]) != -1 || (this.disabledDays).indexOf(this.tmpDate.getDate()) != -1){
+            }else{
+               this.multiple.push(this.tmpDate.getDate()+","+this.tmpDate.getMonth()+","+this.tmpDate.getFullYear()); 
+            }
+          }
+        }else{        
+           this.cf = this.multiple.indexOf(f.text+","+this.currentMonth+","+this.currentYear);
+           if(this.cf < 0)
+            {
+              this.multiple.push(f.text+","+this.currentMonth+","+this.currentYear);
+            }else{
+              target.classList.remove('selected')
+              this.multiple.splice(this.cf,1);
+              this.triggerEvent('multiselect',this.multiple);
+              return;
+            } 
+          if(this.multiple.length > this.multiSelect.max){
+              this.multiple.shift();
+           }             
+        }
+        this.triggerEvent('multiselect',this.multiple);        
+       }
+         this.date = new Date(this.currentYear,this.currentMonth,f.text);      
+         this.set('date', this.date);
+        
+       }     
+    }
+   
+    triggerEvent(e,data){
+		var event = new CustomEvent( e, { detail: { dates: data } } );
+	    this.dispatchEvent(event);   
+    }
+   
+    _setYear(e){
+      this.currentYear = e.model.item;
+      this.generateTable();
+      this.separator = [0,1,2,3,4,5];
+      current_page = 'calendarContent';
+      this.pagination();
+    }
+    _setMonth(e){
+      this.currentMonth = this.months_names.indexOf(e.model.item); 
+      this.generateTable();
+      this.separator = [0,1,2,3,4,5];
+      current_page = 'calendarContent';
+      this.pagination();
+    }
+   
+	_show(e){
+	     current_page = e.target.attributes.type.value; 
+		 this.pagination();
+	}
+	
+    pagination(){
+        var pages = Polymer.dom(this.root).querySelectorAll('.page');
+		for(var i = 0;i<pages.length;i++){
+		     pages[i].style.display = 'none';
+		}       
+        tmpObject =  this.$$('#'+current_page);
+        tmpObject.style.display = 'block';
+        tmpObject.classList.add('scale-up'); 
+        this._once(this._animationEvent, ()=>{
+           (tmpObject).classList.remove('scale-up')     
+        }, tmpObject);  
+
+        pages = null;
+    }
+	
+    _generateYears(min,max,tmpArray){
+	     for(var i=min;i<=max;i++){
+		    tmpArray.push(i);
+		  }
+       this.years  = tmpArray; 				  
+	}
+	
+	_populate(date){      
+	    this.currentMonth = date.getMonth();
+		this.currentYear = date.getFullYear();
+		this.dispatchEvent(new CustomEvent('date-change', { detail : { date: this.date } } ));
+	}
+	
+	dateFormat(date){
+	    if(this.date){
+			return (this.days_names[(this.date).getDay()])	 
+		 }
+	}
+	 
+    monthFormat(){
+     return this.months_names[this.currentMonth]
+    } 
+    yearFormat(){
+      return this.currentYear;
+    }
+	
+	_swipeNextMonth(){
+     this.$.mainContent.classList.add('slide-right') ; 
+     this._once(this._animationEvent, ()=> {
+       this.$.mainContent.classList.remove('slide-right')     
+     },this.$.mainContent);  
+      this.changeView(1);
+	}
+    
+	changeView(x){	    
+		 var tmp = new Date(this.currentYear,this.currentMonth,1);
+		 tmp.setMonth(this.currentMonth+x);
+		 this.currentMonth = tmp.getMonth();
+		 this.currentYear = tmp.getFullYear();
+		 this.dispatchEvent(new CustomEvent('month-change', { detail : { date: this.tmpDate } } ) );
+		 this.generateTable();
+		 this.separator = [0,1,2,3,4,5];
+	 }
+	
+	_once(eventName, callback, node) {
+        function onceCallback() {
+          node.removeEventListener(eventName, onceCallback);
+          callback();
+        }
+        node.addEventListener(eventName, onceCallback);
+    }
+	
+	_swipePrevMonth(){
+
+      this.$.mainContent.classList.add('slide-left') ; 
+       this._once(this._animationEvent, ()=> {
+       this.$.mainContent.classList.remove('slide-left')     
+      }, this.$.mainContent);  
+     this.changeView(-1);
+ 
+	 }
+	 
+	 _getDays(row){
+	   return ((this.days).slice(row*7,(row*7)+7))
+	 }
+    
+	_whichAnimationEnd() {
+        var animations = {
+          'WebkitTransition' : 'webkitAnimationEnd',
+          'MozTransition'    : 'animationend',
+          'OTransition'      : 'oanimationend  oAnimationEnd ',
+          'transition'       : 'animationend'
+        };
+
+        for (var t in animations) {
+          if (this.style[t] !== undefined){
+            return animations[t];
+          }
+        }
+    }  
+}
+
+window.customElements.define(CalendarLite.is, CalendarLite);
---
+++ calendar-lite_polymer1.x/README.md
@@ -0,0 +1,126 @@
+[![Published on webcomponents.org](https://img.shields.io/badge/webcomponents.org-published-blue.svg)](https://beta.webcomponents.org/element/Manohar-Gunturu/calendar-lite)
+
+
+# \<calendar-lite\>
+
+Multi select date picker or calendar
+
+Calendar-lite is a Webcomponent build with Polymer. It gives a nice interface to play with it.
+
+## Features
+
+1. Set min and max date,
+2. Select Multiple dates(consequent or random)
+3. Disable week days(example disable all Sundays)
+4. Disable an array of dates(example 1st and 3rd of this month)
+5. triggers an event on date change(So you can update the value of an input field)
+6. triggers an event on month change(So you can set different disable dates for different months)
+7. Customizable theme
+
+It has two themes one is calendar-lite and another is calendar-lite-dark(dark theme).
+
+<!---
+```
+<custom-element-demo>
+  <template>
+    <script src="../webcomponentsjs/webcomponents-lite.js"></script>
+    <link rel="import" href="calendar-lite.html">
+    <next-code-block></next-code-block>
+  </template>
+</custom-element-demo>
+```
+-->
+```html
+<calendar-lite></calendar-lite>
+```
+
+<!---
+```
+<custom-element-demo>
+  <template>
+    <script src="../webcomponentsjs/webcomponents-lite.js"></script>
+    <link rel="import" href="calendar-lite-dark.html">
+    <next-code-block></next-code-block>
+  </template>
+</custom-element-demo>
+```
+-->
+```html
+<calendar-lite-dark  id="someid"  multi-select='{"max":3,"consequent":true}'></calendar-lite-dark>
+```
+
+
+
+You can attach date-change event listener to it as shown below
+
+```javascript
+    // called whenever a user selects/change a date
+    document.querySelector('#someid').addEventListener('date-change', function (e) {
+        console.log(e.detail.date); //update input values...
+    })
+```
+ 
+You can disable week days by passing an array as shown below.
+```html
+    <calendar-lite id="someid" disabled-week-day='["Fri","Sun"]'></calendar-lite>
+```
+ 
+You can disable a bunch of days by passing an array as shown below.
+
+```html
+    <calendar-lite id="someid" disabled-days="[4,20,27]"></calendar-lite>
+ ```
+ 
+Here you may get a doubt that "How to disable different dates for different months?"
+
+Answer is, you can update the disable dates on `month-change` event as shown below.
+ 
+```javascript
+    document.querySelector('#someid').addEventListener('month-change', function (e) {
+         //takecare month numbering starts from 0
+         if(e.detail.date.getMonth() == 4){
+		        document.querySelector('#someid').disabledDays = [1]
+		      }else{
+		         document.querySelector('#someid').disabledDays = [7,8]
+		      }
+    })
+```
+ 
+You can select multiple days by passing an Object to `multi-select` attribute as shown below.
+
+```html
+    <calendar-lite id="someid" multi-select='{"max":3,"consequent":false}'  disabled-week-day='["Fri"]'  disabled-days="[2,3,4]">
+    </calendar-lite>
+ ```
+ 
+To get the selected multiple dates, use below listener
+
+```javascript
+   document.querySelector('#excalendar').addEventListener('multiselect', function (e) {
+        console.log(e.detail.dates); // array of selected dates
+    })
+```
+ 
+In Object multi-select: `max` is nothing but maximum number of days that can be selected, if `consequent` is true it will select the days in consequent.
+
+you can provide min and max dates, such that calendar-lite will disable the remaining dates.
+```html
+    <calendar-lite id="someid" min-date="2016,12,9" multi-select='{"max":3,"consequent":false}'  disabled-week-day='["Fri"]'  disabled-days="[2,3,4]">
+    </calendar-lite>
+ ```    
+min-date and max-date format should be yyyy-mm-dd.
+
+## To change theme
+
+To change main header color of calendar-lite
+
+```html
+
+    <calendar-lite main-color="#E91E63"  id="someid"></calendar-lite>
+
+```
+You can use dark theme calendar by importing `calendar-lite-dark.html` instead of `calendar-lite.html`.
+
+```html
+    <calendar-lite-dark  id="someid"></calendar-lite-dark>
+ ```
